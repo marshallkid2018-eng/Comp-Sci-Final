bbdf96c5f35c230f0beaa123cfca619655768654
marshallkid2018-eng <marshallkid2018@gmail.com>
2026-05-04 09:55:55 -0500

    Implement movement for Obstacle class
    Added movement functionality to the Obstacle class, allowing it to move back and forth within specified coordinates.

--- Obstacle.java
+++ Obstacle.java
@@ -1,37 +1,55 @@
 import java.awt.Rectangle;
 
 public class Obstacle {
-    
+
     private int x, y, width, height;
+
+    // Movement
+    private boolean moving = false;
+    private int speed = 0;
+    private int startX, endX;
+    private boolean movingRight = true;
+    private int originalX;
 
     public Obstacle(int xpos, int ypos, int w, int h) {
         x = xpos;
         y = ypos;
         width = w;
         height = h;
-    }
-    
-    public void shiftX(int amount) {
-        x -= amount;
+        originalX = xpos;
     }
 
-    // Getters
+    // WORLD COORDINATES ONLY
     public int getX() { return x; }
     public int getY() { return y; }
     public int getW() { return width; }
     public int getH() { return height; }
 
-
-    // Setters
-    public void setX(int xpos) { x = xpos; }
-    public void setY(int ypos) { y = ypos; }
-    public void setW(int w) { width = w; }
-    public void setH(int h) { height = h; }
-    
     public Rectangle getBounds() {
         return new Rectangle(x, y, width, height);
     }
 
+    public void enableMovement(int speed, int startX, int endX) {
+        this.moving = true;
+        this.speed = speed;
+        this.startX = startX;
+        this.endX = endX;
+    }
 
+    public void updateMovement() {
+        if (!moving) return;
 
+        if (movingRight) {
+            x += speed;
+            if (x >= endX) movingRight = false;
+        } else {
+            x -= speed;
+            if (x <= startX) movingRight = true;
+        }
+    }
+
+    public void resetMovement() {
+        x = originalX;
+        movingRight = true;
+    }
 }
