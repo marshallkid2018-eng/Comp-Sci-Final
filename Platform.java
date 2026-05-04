7f6462aa81a7a1930797dfcd3e8f77be75a26469
marshallkid2018-eng <marshallkid2018@gmail.com>
2026-05-04 09:56:13 -0500

    Implement back-and-forth movement for Platform
    Added movement functionality to the Platform class, allowing it to move back and forth within specified coordinates.

--- Platform.java
+++ Platform.java
@@ -1,35 +1,65 @@
 import java.awt.Rectangle;
 
 public class Platform {
-    
+
     private int x, y, width, height;
+
+    // Movement
+    private boolean moving = false;
+    private int speed = 0;
+    private int startX, endX;
+    private boolean movingRight = true;
+    private int originalX;
+    private int lastMovement = 0;
+public int getLastMovement() { return lastMovement; }
+public boolean isMoving() { return moving; }
+
 
     public Platform(int xpos, int ypos, int w, int h) {
         x = xpos;
         y = ypos;
         width = w;
         height = h;
-    }
-    
- // Move platform when camera scrolls
-    public void shiftX(int amount) {
-        x -= amount;
+        originalX = xpos;
     }
 
-    // Getters
+    // WORLD COORDINATES ONLY
     public int getX() { return x; }
     public int getY() { return y; }
     public int getWidth() { return width; }
     public int getHeight() { return height; }
 
-    // Setters
-    public void setX(int xpos) { x = xpos; }
-    public void setY(int ypos) { y = ypos; }
-    public void setWidth(int w) { width = w; }
-    public void setHeight(int h) { height = h; }
-    
     public Rectangle getBounds() {
         return new Rectangle(x, y, width, height);
     }
 
+    // Enable back‑and‑forth movement
+    public void enableMovement(int speed, int startX, int endX) {
+        this.moving = true;
+        this.speed = speed;
+        this.startX = startX;
+        this.endX = endX;
+    }
+
+    public void updateMovement() {
+    lastMovement = 0;
+
+    if (!moving) return;
+
+    if (movingRight) {
+        x += speed;
+        lastMovement = speed;
+        if (x >= endX) movingRight = false;
+    } else {
+        x -= speed;
+        lastMovement = -speed;
+        if (x <= startX) movingRight = true;
+    }
+}
+
+
+    public void resetMovement() {
+        x = originalX;
+        movingRight = true;
+    }
 }
